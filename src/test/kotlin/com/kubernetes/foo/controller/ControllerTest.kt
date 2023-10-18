a9870e49f1969f4f456c9531beb8f6cf2481619d
cheoljin <asddqe111@bbrick.com>
2023-10-18 23:15:40 +0900

    [JenkinsFile] - test 제거

--- src/test/kotlin/com/kubernetes/foo/controller/ControllerTest.kt
+++ src/test/kotlin/com/kubernetes/foo/controller/ControllerTest.kt
@@ -5,11 +5,11 @@ import org.mockito.BDDMockito
 import org.mockito.Mock
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+// @SpringBootTest
 class ControllerTest {
-    @Mock
+    // @Mock
     private lateinit var controller: FooController
-    @Test
+    // @Test
     fun `이 테스트는 무조건 성공한다`() {
         BDDMockito.given(controller.foo()).willReturn("foo")
     }
